22cfd7d784f2299d0f412b22f41689f130921f61
unknown <0000123921@US94659C52B156.am.sony.com>
2016-01-18 09:03:34 -0800

    Fixed codes

---
+++ README.md
@@ -0,0 +1,26 @@
+Sample source code to get application list with LEANBACK_LAUNCHER on AndroidTV.
+=============
+
+## Introduction
+AndroidTV application developer must include the LEANBACK_LAUNCHER intent filter in the AndroidManifest.xml as it is used by Play Store for filtering.
+This source code describes how to get application list with LEANBACK_LAUNCHER on AndroidTV.
+
+## ScreenShot
+
+![Image](https://raw.githubusercontent.com/nehori/java-app-list-androidtv/master/screenshot/1.png)
+
+## License
+Copyright 2016 nehori.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+You may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
--- app/src/main/java/com/nehori/applist/MainActivity.java
+++ app/src/main/java/com/nehori/applist/MainActivity.java
@@ -7,16 +7,15 @@ import java.util.List;
 
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
-import android.view.Menu;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.content.Intent;
 
-
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
     private PackageManager packageMgr;
 
     @Override
@@ -24,45 +23,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<String> appList = new ArrayList<String>();
+        ArrayList<String> appList = new ArrayList<>();
         packageMgr = getPackageManager();
 
-        List<ApplicationInfo> applicationInfo = packageMgr.getInstalledApplications(PackageManager.GET_META_DATA);
-        for (ApplicationInfo info : applicationInfo) {
-            if ((info.flags & ApplicationInfo.FLAG_SYSTEM) == 0
-                    && (info.flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) == 0) {
-                // Downloaded app
-                appList.add(packageMgr.getApplicationLabel(info).toString());
-            }
-        }
-        // Add leanback launchers
-        Intent leanbackLauncherIntent = new Intent(Intent.ACTION_MAIN);
+        // Application on Leanback launcher
+        Intent leanbackLauncherIntent = new Intent("android.intent.action.MAIN");
         leanbackLauncherIntent.addCategory(Intent.CATEGORY_LEANBACK_LAUNCHER);
-        List<ResolveInfo> launchableApps =
-// GET_META_DATA, GET_ACTIVITIES
-                packageMgr.queryIntentActivities(leanbackLauncherIntent, PackageManager.GET_DISABLED_COMPONENTS
-                        | PackageManager.GET_UNINSTALLED_PACKAGES);
-        for (ResolveInfo info : launchableApps) {
-            if (info.activityInfo != null && info.activityInfo.applicationInfo != null) {
-                int flags = info.activityInfo.applicationInfo.flags;
-                final String appName = info.loadLabel(packageMgr).toString();
-                if ((flags & ApplicationInfo.FLAG_SYSTEM) != 0
-                        || (flags & ApplicationInfo.FLAG_UPDATED_SYSTEM_APP) != 0) {
-                    appList.add(appName);
+        List<ResolveInfo> launchableApps = packageMgr.queryIntentActivities(leanbackLauncherIntent,
+				packageMgr.GET_META_DATA | packageMgr.GET_ACTIVITIES );
+          for (ResolveInfo info : launchableApps) {
+            if (info.activityInfo != null) {
+                String appName = info.loadLabel(packageMgr).toString();
+                boolean isGame = false;
+                if (info.activityInfo.applicationInfo.metaData != null) {
+                    isGame = info.activityInfo.applicationInfo.metaData.getBoolean("isGame", false);
                 }
+                if (isGame) {
+                    appName = appName + " (Game Row)";
+                }
+                appList.add(appName);
+                Log.d(TAG, appName);
             }
         }
-        // Add Settings
-        appList.add("Settings");
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1, appList);
         ListView listView = (ListView) findViewById(R.id.listView);
         listView.setAdapter(adapter);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
     }
 }
